e6982aa97a322da0b87ac7bf1e05027f7ab9d6e5
Islam Nabil <islamelgaafary@gmail.com>
2023-05-11 00:15:35 +0300

    [UPDATE] projects files structure

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Islam Elgaafary on 10/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
