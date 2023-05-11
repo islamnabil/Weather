97f049b4fe9188f9d6b6e29cd44d5c988a164cc6
Islam Nabil <islamelgaafary@gmail.com>
2023-05-12 00:59:52 +0300

    [ADD] `ForecastModel`

--- Weather/Weather/Models/Forecast/ForecastModel.swift
+++ Weather/Weather/Models/Forecast/ForecastModel.swift
@@ -6,3 +6,126 @@
 //
 
 import Foundation
+
+struct ForecastResponse: Codable {
+    let list: [Forecast]?
+    let city: City?
+    
+    enum CodingKeys: String, CodingKey {
+        case list
+        case city = "city"
+    }
+}
+
+struct Forecast: Codable {
+    let dt: Int?
+    let main: MainWather?
+    let weather: [Weather]?
+    let clouds: Clouds?
+    let wind: Wind?
+    let visibility: Int?
+    let pop: Double?
+    let rain: Rain?
+    let sys: Sys?
+    let dtTxt: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case dt
+        case main
+        case weather
+        case clouds
+        case wind
+        case visibility
+        case pop
+        case rain
+        case sys
+        case dtTxt = "dt_txt"
+    }
+}
+
+struct MainWather: Codable {
+    let temp: Double?
+    let feelsLike: Double?
+    let tempMin: Double?
+    let tempMax: Double?
+    let pressure: Int?
+    let seaLevel: Int?
+    let grndLevel: Int?
+    let humidity: Int?
+    let tempKf: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+        case humidity
+        case tempKf = "temp_kf"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int?
+    let main: String?
+    let weatherDescription: String?
+    let icon: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+struct Clouds: Codable {
+    let all: Int?
+}
+
+struct Wind: Codable {
+    let speed: Double?
+    let deg: Int?
+    let gust: Double?
+}
+
+struct Rain: Codable {
+    let threeHour: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case threeHour = "3h"
+    }
+}
+
+struct Sys: Codable {
+    let pod: String?
+}
+
+struct City: Codable {
+    let id: Int?
+    let name: String?
+    let coord: Coord?
+    let country: String?
+    let population: Int?
+    let timezone: Int?
+    let sunrise: Int?
+    let sunset: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case coord
+        case country
+        case population
+        case timezone
+        case sunrise
+        case sunset
+    }
+}
+
+struct Coord: Codable {
+    let lat: Double?
+    let lon: Double?
+}
