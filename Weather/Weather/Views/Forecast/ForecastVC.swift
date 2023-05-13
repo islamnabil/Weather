b24c3afcfc647e1222938dac1e841206850240bb
Islam Nabil <islamelgaafary@gmail.com>
2023-05-13 10:42:50 +0300

    [ADD] forecast tableView in `ForecastVC`

--- Weather/Weather/Views/Forecast/ForecastVC.swift
+++ Weather/Weather/Views/Forecast/ForecastVC.swift
@@ -12,15 +12,22 @@ class ForecastVC: UIViewController {
 
     //MARK: - IBOutlets
     @IBOutlet weak var searchBar: UISearchBar!
-   
+    @IBOutlet weak var forecastTableView: UITableView!
+    
     // MARK: - Properties
     private var viewModel: ForecastViewModelProtocol?
     private var bindings = Set<AnyCancellable>()
     private var location: LastKnownLocation?
+    private var forecastList: [Forecast] = [] {
+        didSet {
+            forecastTableView.reloadData()
+        }
+    }
     
     //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupForecastTable()
         setupBindings()
         if let location {
             viewModel?.fetchForecast(lat: location.lat, lon: location.long)
@@ -49,14 +56,36 @@ class ForecastVC: UIViewController {
     
     
     // MARK: - Private Methods
+    private func  setupForecastTable() {
+        forecastTableView.register(UINib(nibName: "\(ForecastTableCell.self)", bundle: nil), forCellReuseIdentifier: "\(ForecastTableCell.self)")
+        forecastTableView.delegate = self
+        forecastTableView.dataSource = self
+    }
+    
     private func setupBindings() {
         viewModel?.forecast
             .receive(on: RunLoop.main)
-            .sink { _ in
-                let x = self.viewModel?.forecast.value
-                
+            .sink { [weak self] in
+                if let forecastList = $0?.list {
+                    self?.forecastList = forecastList
+                }
             }
             .store(in: &bindings)
     }
 
+}
+
+extension ForecastVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        forecastList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(ForecastTableCell.self)", for: indexPath) as? ForecastTableCell else { return UITableViewCell() }
+        let forecast = forecastList[indexPath.row]
+        cell.config(time: forecast.dtTxt ?? "", weatherCondition: forecast.weather?.first?.main ?? "", temp: forecast.main?.temp ?? 0.0)
+        cell.selectionStyle = .none
+        return cell
+    }
+    
 }
