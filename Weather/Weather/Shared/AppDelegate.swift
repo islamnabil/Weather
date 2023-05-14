6db771d32965df91ddd4028104ac45896beb0be8
Islam Nabil <islamelgaafary@gmail.com>
2023-05-14 23:41:30 +0300

    [ADD] `IQKeyboardManagerSwift`

--- Weather/Weather/Shared/AppDelegate.swift
+++ Weather/Weather/Shared/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Keyboard Configs
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.keyboardDistanceFromTextField = 25
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         return true
     }
 
