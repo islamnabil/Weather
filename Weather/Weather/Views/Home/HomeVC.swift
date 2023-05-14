69fce82581ab1aeaf9029cb986180777cd6c3fc1
Islam Nabil <islamelgaafary@gmail.com>
2023-05-14 23:30:04 +0300

    [ADD] fetching current location weather in `HomeVC`

--- Weather/Weather/Views/Home/HomeVC.swift
+++ Weather/Weather/Views/Home/HomeVC.swift
@@ -6,27 +6,40 @@
 //
 
 import UIKit
+import Combine
 
 class HomeVC: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
     // MARK: - Properties
     private var lastKnownLocation: LastKnownLocation?
+    private var viewModel: CurrentWeatherViewModelProtocol?
+    private var bindings = Set<AnyCancellable>()
+    private var currentWeather: Weather? {
+        didSet {
+            updateViewsData()
+        }
+    }
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         LocationManager.shared.setupLocation()
         LocationManager.shared.delegate = self
+        viewModel = CurrentWeatherViewModel(provider: WeatherAPI())
+        setupBindings()
     }
     
-
+    // MARK: - IBActions
     @IBAction func forecast(_ sender: Any) {
         if let lastKnownLocation {
             let vc = ForecastVC.create(location: lastKnownLocation, viewModel: ForecastViewModel(provider: WeatherAPI()))
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-    
     
     @IBAction func currentWeather(_ sender: Any) {
         if let lastKnownLocation {
@@ -35,12 +48,29 @@ class HomeVC: UIViewController {
         }
     }
     
+    // MARK: - Private Methods
+    private func setupBindings() {
+        viewModel?.weather
+            .receive(on: RunLoop.main)
+            .sink { [weak self] in
+                if let weather = $0 {
+                    self?.currentWeather = weather
+                }
+            }
+            .store(in: &bindings)
+    }
+    
+    private func updateViewsData() {
+        locationNameLabel.text = currentWeather?.name ?? ""
+        tempLabel.text = "\(currentWeather?.main.temperature ?? 0.0)"
+    }
 }
 
 // MARK: - LocationManagerProtocol
 extension HomeVC: LocationManagerProtocol {
     func locationFetched(location: LastKnownLocation) {
         lastKnownLocation  = location
+        viewModel?.fetchCurrentWeather(lat: location.lat, lon: location.long, unit: .Fahrenheit)
     }
     
 }
